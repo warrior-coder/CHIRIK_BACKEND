a36f5e30b2643b7a89c3a12b68e5e65c4f6515b7
warrior-coder <gudkou2003@gmail.com>
2023-03-08 18:21:52 +0300

    Add creating and deleting of roles

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
 
+import { CreateRoleDto } from './dto/create-role.dto';
 import { RolesService } from './roles.service';
 
 @Controller('/roles')
@@ -9,5 +10,22 @@ export class RolesController {
     @Get('/')
     public getAllRoles() {
         return this.rolesService.getAllRoles();
+    }
+
+    @Get('/:roleId')
+    public getRoleById(@Param('roleId', ParseIntPipe) roleId: number) {
+        return this.rolesService.getRoleById(roleId);
+    }
+
+    @Post('/')
+    public createRole(@Body() createRoleDto: CreateRoleDto) {
+        return this.rolesService.createRole(createRoleDto);
+    }
+
+    @Delete('/:roleId')
+    public async deleteRoleById(@Param('roleId', ParseIntPipe) roleId: number) {
+        const role = await this.rolesService.getRoleById(roleId);
+
+        return this.rolesService.deleteRole(role);
     }
 }
--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { NestPgPool, PgConnection } from 'nest-pg';
 
-import { RolesEntity } from 'src/users/entities/roles.entity';
+import { CreateRoleDto } from './dto/create-role.dto';
+import { RolesEntity } from './entities/roles.entity';
 
 @Injectable()
 export class RolesService {
@@ -12,5 +13,52 @@ export class RolesService {
             SELECT r.*
             FROM public.roles AS r;
         `);
+    }
+
+    public async getRoleById(roleId: number): Promise<RolesEntity | null> {
+        const queryResultRows = await this.pgConnection.rows<RolesEntity>(
+            `
+                SELECT r.*
+                FROM public.roles AS r
+                WHERE r.id = $1::INT
+            `,
+            [roleId],
+        );
+        const role: RolesEntity | undefined = queryResultRows[0];
+
+        if (!role) {
+            return null;
+        }
+
+        return role;
+    }
+
+    public async createRole(createRoleDto: CreateRoleDto): Promise<RolesEntity> {
+        const insertedRows: RolesEntity[] = await this.pgConnection.rows<RolesEntity>(
+            `
+                INSERT INTO roles("value")
+                VALUES ($1::VARCHAR(16))
+                RETURNING id, "value";
+            `,
+            [createRoleDto.value],
+        );
+        const role: RolesEntity = insertedRows[0];
+
+        return role;
+    }
+
+    public deleteRole(role: RolesEntity): Promise<any> {
+        if (!role) {
+            throw new NotFoundException('Role not found.');
+        }
+
+        return this.pgConnection.rows<any>(
+            `
+                DELETE
+                FROM roles AS r
+                WHERE r.id = $1::INT;
+            `,
+            [role.id],
+        );
     }
 }
